ddeea037b8fd663abadb1d0cc3542f4b748e03d5
igor <vigor56@list.ru>
2014-04-17 22:29:41 +0400

    added Repository.cs

---
+++ Solyanka.Domain/Core/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Solyanka.Domain.Core.WhereSpec;
+
+namespace Solyanka.Domain.Core.Repositories
+{
+    public class Repository : IRepository 
+    {
+        private readonly DataContext _context;
+       
+        public Repository(DataContext context)
+        {
+            this._context = context;
+        }
+
+       
+        public void Save<TEntity>(TEntity entity) where TEntity : class
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void SaveOrUpdate<TEntity>(TEntity entity) where TEntity : class
+        {
+            _context.SaveChanges();
+        }
+
+        public IQueryable<TEntity> Query<TEntity>(Specification<TEntity> whereSpecification = null) where TEntity : class
+        {
+            return GetQuery(whereSpecification);
+        }
+
+
+        IQueryable<TEntity> GetQuery<TEntity>(Specification<TEntity> whereSpecification) where TEntity : class
+        {
+            IQueryable<TEntity> dbSet = _context.Set<TEntity>();
+            
+            if (whereSpecification != null && whereSpecification.IsSatisfiedBy() != null)
+                dbSet = dbSet.Where(whereSpecification.IsSatisfiedBy());
+
+
+            return dbSet;
+        }
+    }
+    
+}
