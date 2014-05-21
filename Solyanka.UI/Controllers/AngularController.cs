acf81c494f146926f43a3718934b6a902b743eb0
eauk <vigor56@mail.ru>
2014-05-21 09:23:57 +0400

    added Angular js and the first angular page

---
+++ Solyanka.UI/Controllers/AngularController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Solyanka.UI.Controllers
+{
+    public class AngularController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Solyanka.UI/Scripts/app.js
@@ -0,0 +1,35 @@
+﻿
+
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'some descriptions',
+            canPurchase: true,
+            soldOut: false,
+        },
+        {
+            name: 'Dodecahedron2',
+            price: 3.87,
+            description: 'some descriptions2',
+            canPurchase: true,
+            soldOut: true,
+        },
+        {
+            name: 'Dodecahedron3',
+            price: 1.17,
+            description: 'some descriptions3',
+            canPurchase: true,
+            soldOut: false,
+        }];
+})();
+
+
+
